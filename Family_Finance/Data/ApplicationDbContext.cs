5e71661b4f2ae6785d1d4e67461c1127d7f359ed
michalkuzdowicz <michalkuzdowicz@gmail.com>
2024-12-05 17:49:44 +0100

    Dodaj pliki projektów.

---
+++ Family_Finance/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Family_Finance.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Family_Finance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Family_Finance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
